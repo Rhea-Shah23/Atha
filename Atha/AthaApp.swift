05bfadca68b51f8a0bdbf112cd396d6f00410c94
Rhea Shah <codingrhea@gmail.com>
2026-01-03 13:37:43 -0600

    Initial Commit

---
+++ Atha/AthaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AthaApp.swift
+//  Atha
+//
+//  Created by Rhea Shah on 1/3/26.
+//
+
+import SwiftUI
+
+@main
+struct AthaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
